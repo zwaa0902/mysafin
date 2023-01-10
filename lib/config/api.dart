93b8c9a3add268aa591f1bb1a067cfaf77ff86e1
Sơn Hoàng <ngatt@safin.vn>
2023-01-10 16:11:13 +0700

    Add file api

--- lib/config/api.dart
+++ lib/config/api.dart
@@ -1,1 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:nd_dio_utils/nd_dio_utils.dart';
+import 'config.dart';
 
+Dio? _loginDio;
+
+Dio getAuth() {
+  if (_loginDio == null) {
+    _loginDio = Dio(BaseOptions(baseUrl: baseUrl));
+    enablePrettyLogger([_loginDio!]);
+    enableCurlLogger([_loginDio!]);
+  }
+  return _loginDio!;
+}
